05b69d7d814dcd771d559f479d002a89daafb8ba
blep111 <krexceloso@gmail.com>
2025-11-29 21:12:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,1 @@
-# config.py
-
-# Facebook App credentials
-FACEBOOK_CLIENT_ID = 'your_facebook_app_id'
-FACEBOOK_CLIENT_SECRET = 'your_facebook_app_secret'
-REDIRECT_URI = 'http://localhost:5000/auth/facebook/callback'  # OAuth callback URL
+{}
